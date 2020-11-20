a7b1aa40dd82c5478a096fa33a1fab7d24ebe09f
Pratik <pchy.393@gmail.com>
2020-11-20 11:08:33 +0530

    project structure modified

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+
+import Navigation from "./navigation";
+import { Block } from "./components";
+
+// import all used images
+const images = [
+  require("./assets/icons/back.png"),
+  require("./assets/icons/plants.png"),
+  require("./assets/icons/seeds.png"),
+  require("./assets/icons/flowers.png"),
+  require("./assets/icons/sprayers.png"),
+  require("./assets/icons/pots.png"),
+  require("./assets/icons/fertilizers.png"),
+  require("./assets/images/plants_1.png"),
+  require("./assets/images/plants_2.png"),
+  require("./assets/images/plants_3.png"),
+  require("./assets/images/explore_1.png"),
+  require("./assets/images/explore_2.png"),
+  require("./assets/images/explore_3.png"),
+  require("./assets/images/explore_4.png"),
+  require("./assets/images/explore_5.png"),
+  require("./assets/images/explore_6.png"),
+  require("./assets/images/illustration_1.png"),
+  require("./assets/images/illustration_2.png"),
+  require("./assets/images/illustration_3.png"),
+  require("./assets/images/avatar.png")
+];
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  };
+
+  handleResourcesAsync = async () => {
+    // we're caching all the images
+    // for better performance on the app
+
+    const cacheImages = images.map(image => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+
+    return Promise.all(cacheImages);
+  };
+
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this.handleResourcesAsync}
+          onError={error => console.warn(error)}
+          onFinish={() => this.setState({ isLoadingComplete: true })}
+        />
+      );
+    }
+
+    return (
+      <Block white>
+        <Navigation />
+      </Block>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ navigation/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Image } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import Welcome from "../screens/Welcome";
+import Login from "../screens/Login";
+import SignUp from "../screens/SignUp";
+import Forgot from "../screens/Forgot";
+import Settings from "../screens/Settings";
+
+import { theme } from "../constants";
+
+const screens = createStackNavigator(
+  {
+    Welcome,
+    Login,
+    SignUp,
+    Forgot,
+    Settings
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        height: theme.sizes.base * 4,
+        backgroundColor: theme.colors.white, // or 'white
+        borderBottomColor: "transparent",
+        elevation: 0 // for android
+      },
+      headerBackImage: <Image source={require("../assets/icons/back.png")} />,
+      headerBackTitle: null,
+      headerLeftContainerStyle: {
+        alignItems: "center",
+        marginLeft: theme.sizes.base * 2,
+        paddingRight: theme.sizes.base
+      },
+      headerRightContainerStyle: {
+        alignItems: "center",
+        paddingRight: theme.sizes.base
+      }
+    }
+  }
+);
+
+export default createAppContainer(screens);
